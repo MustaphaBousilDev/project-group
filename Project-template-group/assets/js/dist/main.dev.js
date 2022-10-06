c0c6c11370e59f520602405060da39395fb3b280
unknown <bousilmustapha@gmail.com>
2022-10-06 17:27:59 +0100

    complete template work 5

--- Project-template-group/assets/js/dist/main.dev.js
+++ Project-template-group/assets/js/dist/main.dev.js
@@ -13,4 +13,47 @@ var showMenu = function showMenu(toggleId, navId) {
   }
 };
 
-showMenu('nav-toggle', 'nav-menu');
+showMenu('nav-toggle', 'nav-menu'); //////////////////////////////////////////////////
+//remove list ul if click li
+
+var navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  var navMenu = document.getElementById('nav-menu'); //when fucking click on each nav__link , we fucking remove the show-menu
+
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(function (link) {
+  link.addEventListener('click', linkAction);
+}); ///////////////////////////////////////////////////
+
+var themeButton = document.getElementById('theme-button');
+var darkTheme = 'dark-theme';
+var iconTheme = 'bx-toggle-right'; //Previously selected topic (if user selected)
+
+var selectedTheme = localStorage.getItem('selected-theme');
+var selectedIcon = localStorage.getItem('selected-icon'); //we obtain the current theme that the interface has by validating the dark-theme class
+
+var getCurrentTheme = function getCurrentTheme() {
+  return document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+};
+
+var getCurrentIcon = function getCurrentIcon() {
+  return themeButton.classList.contains(iconTheme) ? 'box-toggle-left' : 'bx-toggle-right';
+};
+
+if (selectedTheme) {
+  //if the validation is fulfilled , we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', function () {
+  //Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme); //we save the theme and the current icon that the user chose
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
--- Project-template-group/assets/js/main.js
+++ Project-template-group/assets/js/main.js
@@ -12,3 +12,40 @@ const showMenu=(toggleId,navId)=>{
 }
 
 showMenu('nav-toggle','nav-menu')
+//////////////////////////////////////////////////
+//remove list ul if click li
+var navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  var navMenu = document.getElementById('nav-menu'); //when fucking click on each nav__link , we fucking remove the show-menu
+   navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((link) =>{
+    link.addEventListener('click', linkAction);
+});
+
+///////////////////////////////////////////////////
+const themeButton=document.getElementById('theme-button')
+const darkTheme='dark-theme'
+const iconTheme='bx-toggle-right'
+//Previously selected topic (if user selected)
+const selectedTheme=localStorage.getItem('selected-theme')
+const selectedIcon=localStorage.getItem('selected-icon')
+//we obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme=()=>document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon=()=>themeButton.classList.contains(iconTheme) ? 'box-toggle-left'  : 'bx-toggle-right'
+if(selectedTheme){
+    //if the validation is fulfilled , we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme==='dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon==='bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click',()=>{
+    //Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //we save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+})
