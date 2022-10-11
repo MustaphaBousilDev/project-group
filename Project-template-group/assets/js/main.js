5c3612747b83f4d638e0622463d5708220e45e74
unknown <bousilmustapha@gmail.com>
2022-10-11 10:03:03 +0100

    finally

--- Project-template-group/assets/js/main.js
+++ Project-template-group/assets/js/main.js
@@ -75,21 +75,4 @@ window.addEventListener('scroll',scrollUp)
 
 
 
-//-----------------------------------------------------
-//Links scrolling 
-const sections=document.querySelectorAll('section[id]')
-function scrollActive(){
-    let scrollY=window.pageYOffset
-    console.log(scrollY)
-    sections.forEach((current)=>{
-        const sectionHeight=current.offsetHeight,
-        sectionTop=current.offsetTop - 50,
-        sectionId=current.getAttribute('id')
-        if(scrollY > sectionTop && scrollY <=sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll',scrollActive)
+
